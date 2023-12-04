acdabfba088b7c11dda52eb1ab7488068c832a6b
İsmail Harmanda <iharmanda@hotmail.com>
2023-12-04 22:49:48 +0300

    move the bird by touching on screen

--- AngryBird/GameScene.swift
+++ AngryBird/GameScene.swift
@@ -19,6 +19,8 @@ class GameScene: SKScene {
     var box4 = SKSpriteNode()
     var box5 = SKSpriteNode()
     
+    var gameStarted = false
+    
     override func didMove(to view: SKView) {
         
 //      create a new bird via code
@@ -111,12 +113,53 @@ class GameScene: SKScene {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 100))
-        bird.physicsBody?.affectedByGravity = true
+//        bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 100))
+//        bird.physicsBody?.affectedByGravity = true
+        
+        if !gameStarted {
+            
+            if let touch = touches.first {
+                
+                let touchLocation = touch.location(in: self)
+                let touchNodes = nodes(at: touchLocation)
+                
+                if touchNodes.isEmpty == false {
+                    for node in touchNodes {
+                        if let sprite = node as? SKSpriteNode {
+                            
+                            if sprite === bird {
+                                bird.position = touchLocation
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
         
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if !gameStarted {
+            
+            if let touch = touches.first {
+                
+                let touchLocation = touch.location(in: self)
+                let touchNodes = nodes(at: touchLocation)
+                
+                if touchNodes.isEmpty == false {
+                    for node in touchNodes {
+                        if let sprite = node as? SKSpriteNode {
+                            
+                            if sprite === bird {
+                                bird.position = touchLocation
+                            }
+                        }
+                    }
+                }
+            }
+        }
         
     }
     
